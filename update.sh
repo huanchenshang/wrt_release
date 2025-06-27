66d341df9c2f0f18c711b98553c8e68b25c812e5
huanchenshang <840943839@qq.com>
2025-06-27 19:34:02 +0800

    Update update.sh
    Signed-off-by: huanchenshang <840943839@qq.com>

--- update.sh
+++ update.sh
@@ -910,7 +910,6 @@ main() {
     update_script_priority
     fix_easytier
     install_update_geodata_script
-    replace_nginx_config
     update_package "runc" "releases" "v1.2.6"
     update_package "containerd" "releases" "v1.7.27"
     update_package "docker" "tags" "v28.2.2"
