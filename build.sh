e502ebc7f465edc881055ffe91fee7b7668fecf2
huanchenshang <840943839@qq.com>
2025-08-22 23:37:23 +0800

    Update build.sh
    Signed-off-by: huanchenshang <840943839@qq.com>

--- build.sh
+++ build.sh
@@ -25,20 +25,6 @@ read_ini_by_key() {
     awk -F"=" -v key="$key" '$1 == key {print $2}' "$INI_FILE"
 }
 
-# 移除 uhttpd 依赖
-# 当启用luci-app-quickfile插件时，表示启动nginx，所以移除luci对uhttp(luci-light)的依赖
-remove_uhttpd_dependency() {
-    local config_path="$BASE_PATH/$BUILD_DIR/.config"
-    local luci_makefile_path="$BASE_PATH/$BUILD_DIR/feeds/luci/collections/luci/Makefile"
-
-    if grep -q "CONFIG_PACKAGE_luci-app-quickfile=y" "$config_path"; then
-        if [ -f "$luci_makefile_path" ]; then
-            sed -i '/luci-light/d' "$luci_makefile_path"
-            echo "Removed uhttpd (luci-light) dependency as luci-app-quickfile (nginx) is enabled."
-        fi
-    fi
-}
-
 # 添加内核配置以支持 dae
 cat_kernel_config() {
 conde_file="$BASE_PATH/$BUILD_DIR/target/linux/qualcommax/ipq60xx/config-default"
